591c310201bb751348ca36dd64fea78cf0ff2f89
Karthik <56579449+karthik9728@users.noreply.github.com>
2022-04-28 10:04:03 +0530

    Controller

--- JWT.TokenV2/JWT.TokenV2/Controllers/UsersController.cs
+++ JWT.TokenV2/JWT.TokenV2/Controllers/UsersController.cs
@@ -8,6 +8,7 @@ using Microsoft.AspNetCore.Mvc;
 namespace JWT.TokenV2.Controllers
 {
     [Route("api/[controller]")]
+    //[Route("api/[controller]/[action]")]
     [ApiController]
     public class UsersController : ControllerBase
     {
